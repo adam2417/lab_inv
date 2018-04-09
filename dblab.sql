4978a023d33f16b48bb085fce927d8b14819ac30
adam2417 <38124086+adam2417@users.noreply.github.com>
2018-04-09 15:18:47 +0700

    initial db

---
+++ dblab.sql
@@ -0,0 +1,89 @@
+-- phpMyAdmin SQL Dump
+-- version 4.5.1
+-- http://www.phpmyadmin.net
+--
+-- Host: 127.0.0.1
+-- Generation Time: Apr 09, 2018 at 10:04 AM
+-- Server version: 10.1.19-MariaDB
+-- PHP Version: 5.6.28
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `dblab`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tb_adminlogin`
+--
+-- in use(#1932 - Table 'dblab.tb_adminlogin' doesn't exist in engine)
+-- Error reading data: (#1932 - Table 'dblab.tb_adminlogin' doesn't exist in engine)
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tb_config`
+--
+-- in use(#1932 - Table 'dblab.tb_config' doesn't exist in engine)
+-- Error reading data: (#1932 - Table 'dblab.tb_config' doesn't exist in engine)
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tb_dokter`
+--
+-- in use(#1932 - Table 'dblab.tb_dokter' doesn't exist in engine)
+-- Error reading data: (#1932 - Table 'dblab.tb_dokter' doesn't exist in engine)
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tb_inventaris`
+--
+-- in use(#1932 - Table 'dblab.tb_inventaris' doesn't exist in engine)
+-- Error reading data: (#1932 - Table 'dblab.tb_inventaris' doesn't exist in engine)
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tb_pasien`
+--
+-- in use(#1932 - Table 'dblab.tb_pasien' doesn't exist in engine)
+-- Error reading data: (#1932 - Table 'dblab.tb_pasien' doesn't exist in engine)
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tb_pegawai`
+--
+-- in use(#1932 - Table 'dblab.tb_pegawai' doesn't exist in engine)
+-- Error reading data: (#1932 - Table 'dblab.tb_pegawai' doesn't exist in engine)
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tb_pemeriksaan`
+--
+-- in use(#1932 - Table 'dblab.tb_pemeriksaan' doesn't exist in engine)
+-- Error reading data: (#1932 - Table 'dblab.tb_pemeriksaan' doesn't exist in engine)
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tb_praktikum`
+--
+-- in use(#1932 - Table 'dblab.tb_praktikum' doesn't exist in engine)
+-- Error reading data: (#1932 - Table 'dblab.tb_praktikum' doesn't exist in engine)
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
